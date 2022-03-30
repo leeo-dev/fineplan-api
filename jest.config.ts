69f8b346d6d06d220f995e754e93dbff8dddef6c
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-30 11:41:32 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
--- src/testoutro.ts
+++
@@ -1,2 +0,0 @@
-function leo (): void {}
-leo()
