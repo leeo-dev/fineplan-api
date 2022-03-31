2797635856a35d5b6cf80e76b6ef8ab0b5297eb0
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-30 23:06:35 -0300

    refactor: ensure SignController implements correct protocols

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,15 @@
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+import { Controller } from '@/presentation/protocols/controller'
 import { badRequest } from '@/presentation/helpers/http/http'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
-export class SignUpController {
-  handle (httpRequest: any): any {
+export class SignUpController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const { username, password } = httpRequest.body
     if (!username) return badRequest(new MissingParamError('username'))
     if (!password) return badRequest(new MissingParamError('password'))
+    return {
+      statusCode: 200,
+      body: ''
+    }
   }
 }
