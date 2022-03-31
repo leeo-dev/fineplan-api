762a8cf8b2e0f262268fd72358cf5f34159be232
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-30 22:57:57 -0300

    feat: ensure SignUpController use error personalized

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { badRequest } from '@/presentation/helpers/http/http'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
 export class SignUpController {
   handle (httpRequest: any): any {
     const { username, password } = httpRequest.body
-    if (!username) return badRequest(new Error('MissingParam: username'))
-    if (!password) return badRequest(new Error('MissingParam: password'))
+    if (!username) return badRequest(new MissingParamError('username'))
+    if (!password) return badRequest(new MissingParamError('password'))
   }
 }
