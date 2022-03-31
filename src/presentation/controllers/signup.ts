a2c901d49c18ff624b23f50a153b2b58ab893686
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-03-30 23:16:58 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,9 +4,10 @@ import { badRequest } from '@/presentation/helpers/http/http'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
 export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const { username, password } = httpRequest.body
-    if (!username) return badRequest(new MissingParamError('username'))
-    if (!password) return badRequest(new MissingParamError('password'))
+    const requiredFields = ['username', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+    }
     return {
       statusCode: 200,
       body: ''
