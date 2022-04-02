30cfcc50794e1bf28391600e5b2f287644730363
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-02 18:58:30 -0300

    feat: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,29 +1,33 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
-import { badRequest, forbidden } from '@/presentation/helpers/http/http'
+import { badRequest, forbidden, serverError } from '@/presentation/helpers/http/http'
 import { UsernameInUseError } from '@/presentation/errors/username-in-use-error'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { LengthParamError } from '../errors/length-param-error'
 import { AddAccount } from '@/domain/usecases/account/add-account'
 export class SignUpController implements Controller {
   constructor (private readonly addAccount: AddAccount) {}
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['username', 'password']
-    for (const field of requiredFields) {
-      const lengthField = httpRequest.body[field]?.length
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+  async handle (httpRequest: HttpRequest): Promise <HttpResponse> {
+    try {
+      const requiredFields = ['username', 'password']
+      for (const field of requiredFields) {
+        const lengthField = httpRequest.body[field]?.length
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+        if (lengthField < 3 || lengthField > 25) {
+          return badRequest(new LengthParamError(field, 3, 25))
+        }
       }
-      if (lengthField < 3 || lengthField > 25) {
-        return badRequest(new LengthParamError(field, 3, 25))
+      const { username, password } = httpRequest.body
+      const id = await this.addAccount.add({ username, password })
+      if (!id) return forbidden(new UsernameInUseError())
+      return {
+        statusCode: 200,
+        body: ''
       }
-    }
-    const { username, password } = httpRequest.body
-    const id = await this.addAccount.add({ username, password })
-    if (!id) return forbidden(new UsernameInUseError())
-    return {
-      statusCode: 200,
-      body: ''
+    } catch (error) {
+      return serverError()
     }
   }
 }
--- src/presentation/helpers/http/http.ts
+++ src/presentation/helpers/http/http.ts
@@ -1,8 +1,15 @@
-export const badRequest = (error: Error): any => ({
+import { ServerError } from '@/presentation/errors/server-error'
+import { HttpResponse } from '@/presentation/protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
-export const forbidden = (error: Error): any => ({
+export const forbidden = (error: Error): HttpResponse => ({
   statusCode: 403,
   body: error
+})
+export const serverError = (): HttpResponse => ({
+  statusCode: 403,
+  body: new ServerError()
 })
