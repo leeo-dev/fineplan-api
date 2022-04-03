40457a579905f869b940790568e9833456119e3a
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-03 11:01:27 -0300

    chore: remove protcols imports from collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*-protocols.ts', '!<rootDir>/src/**/protocols/**'],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
