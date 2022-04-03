bd11e25a6fdd1577e3b14a2fad4d7b0a0b1b7ab7
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-03 16:47:20 -0300

    feat: ensure DbAddAccount calls Hasher with correct password

--- src/data/usecases/account/add-account/db-add-account.ts
+++ src/data/usecases/account/add-account/db-add-account.ts
@@ -1,12 +1,18 @@
+import { Hasher } from '@/data/protocols/hasher'
 import { LoadUserByUsernameRepository } from '@/data/protocols/load-user-by-username-repository'
 import { AddAccount, AddAccountParams } from '@/domain/usecases/account/add-account'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly loadUserByUsername: LoadUserByUsernameRepository) {}
-  async add (accountParams: AddAccountParams): Promise<string | null> {
-    const account = await this.loadUserByUsername.loadByUsername(accountParams.username)
-    if (account) return null
+  constructor (
+    private readonly loadUserByUsername: LoadUserByUsernameRepository,
+    private readonly hasher: Hasher
+  ) {}
 
+  async add (accountParams: AddAccountParams): Promise<string | null> {
+    const { username, password } = accountParams
+    const account = await this.loadUserByUsername.loadByUsername(username)
+    if (account) return null
+    await this.hasher.hash(password)
     return 'any'
   }
 }
