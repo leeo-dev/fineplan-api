4f8fb517706ac60ab3b812fc16a8b8a753bab164
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-03 10:30:36 -0300

    test: ensure DbAddAccount calls LoadUserByUsernameRepository with correct username

---
+++ src/data/usecases/account/add-account/db-add-account.spec.ts
@@ -0,0 +1,46 @@
+
+import { DbAddAccount } from './db-add-account'
+import { LoadUserByUsernameRepository } from '@/data/protocols/load-user-by-username-repository'
+import { AccountModel } from '@/domain/models/account'
+import { AddAccountParams } from '@/domain/usecases/account/add-account'
+
+const mockAccountParams = (): AddAccountParams => ({
+  username: 'any_username',
+  password: 'any_password'
+})
+
+const mockAccount = (): AccountModel => ({
+  id: 'any_id',
+  username: 'any_username',
+  password: 'any_password'
+})
+
+const mockLoadUserByUsername = (): LoadUserByUsernameRepository => {
+  class LoadUserByUsernameStub implements LoadUserByUsernameRepository {
+    async loadByUsername (username: string): Promise<AccountModel> {
+      return await Promise.resolve(mockAccount())
+    }
+  }
+
+  return new LoadUserByUsernameStub()
+}
+
+type SutType = {
+  sut: DbAddAccount
+  loadUserByUsernameStub: LoadUserByUsernameRepository
+}
+
+const makeSut = (): SutType => {
+  const loadUserByUsernameStub = mockLoadUserByUsername()
+  const sut = new DbAddAccount(loadUserByUsernameStub)
+  return { sut, loadUserByUsernameStub }
+}
+
+describe('DbAddAccount UseCase', () => {
+  test('Should call LoadUserByUsername with correct username', async () => {
+    const { sut, loadUserByUsernameStub } = makeSut()
+    const loadByUsername = jest.spyOn(loadUserByUsernameStub, 'loadByUsername')
+    await sut.add(mockAccountParams())
+    expect(loadByUsername).toHaveBeenCalledWith(mockAccountParams().username)
+  })
+})
