50975cdf4498360c471b41b0a72541fc6d64fab9
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-04 09:45:04 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct string

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,10 @@
+import { Hasher } from '@/data/protocols/hasher'
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter implements Hasher {
+  constructor (private readonly salt: number) {}
+  async hash (value: string): Promise<string> {
+    await bcrypt.hash(value, this.salt)
+    return await Promise.resolve('anm')
+  }
+}
