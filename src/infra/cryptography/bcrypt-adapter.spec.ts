6cb8046443efce7a0348859c1d00d1e8f63a1bbe
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-04 09:45:18 -0300

    test: ensure BcryptAdapter calls bcrypt with correct string

---
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,28 @@
+import { BcryptAdapter } from './bcrypt-adapter'
+import bcrypt from 'bcrypt'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hash')
+  }
+}))
+
+type SutTypes = {
+  sut: BcryptAdapter
+  salt: number
+}
+
+const makeSut = (): SutTypes => {
+  const salt = 12
+  const sut = new BcryptAdapter(salt)
+  return { sut, salt }
+}
+
+describe('Bcrypt Adapter', () => {
+  test('should call hash with correct value', async () => {
+    const { sut, salt } = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.hash('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+})
