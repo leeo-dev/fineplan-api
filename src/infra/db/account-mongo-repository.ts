dac46c5ddf0393e8b6a66768799deecdd5264149
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-05 09:16:04 -0300

    feat: ensure AccountMongoRepository return an account on loadByUsername( success)

--- src/infra/db/account-mongo-repository.ts
+++ src/infra/db/account-mongo-repository.ts
@@ -1,10 +1,18 @@
 import { AddAccountRepository } from '@/data/protocols/add-account-repository'
+import { LoadUserByUsernameRepository } from '@/data/protocols/load-user-by-username-repository'
+import { AccountModel } from '@/domain/models/account'
 import { AddAccountParams } from '@/domain/usecases/account/add-account'
-import { MongoHelper } from '../helpers/mongo-helper'
-export class AddAccountMongoRepository implements AddAccountRepository {
+import { MongoHelper } from '@/infra/helpers/mongo-helper'
+export class AddAccountMongoRepository implements AddAccountRepository, LoadUserByUsernameRepository {
   async add (addAccount: AddAccountParams): Promise<string> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const accountId = await accountCollection.insertOne(addAccount)
     return String(accountId.insertedId)
+  }
+
+  async loadByUsername (username: string): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const accountMongo = await accountCollection.findOne({ username })
+    return accountMongo && MongoHelper.map(accountMongo)
   }
 }
