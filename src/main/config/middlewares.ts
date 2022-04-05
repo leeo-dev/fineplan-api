1bb2f64b339a12dd3c1af0d8ce7b0b54d897e7dc
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-05 09:58:04 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,7 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/middleware/body-parser'
+import { cors } from '@/main/middleware/cors'
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
--- src/main/middleware/body-parser.test.ts
+++ src/main/middleware/body-parser.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import app from '../config/app'
+import app from '@/main/config/app'
 
 describe('Body Parse Middleware', () => {
   test('Should parse body to json', async () => {
