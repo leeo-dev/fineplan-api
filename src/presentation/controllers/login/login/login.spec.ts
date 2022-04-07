eec51d44aeaf4d1d4113370040887583b1d83317
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-07 11:30:33 -0300

    test: ensure LoginController returns 400 if no username is provided

---
+++ src/presentation/controllers/login/login/login.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test, describe } from '@jest/globals'
+import { LoginController } from './login'
+import { MissingParamError } from '../../../errors'
+
+type SutTypes = {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return { sut }
+}
+
+describe('Login Controller', () => {
+  test('should Login Controller returns 400 if no username is provided ', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('username'))
+  })
+})
