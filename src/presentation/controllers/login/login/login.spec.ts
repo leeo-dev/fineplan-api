8c813f757601a16429e380c7be2a47d9aabf000e
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-08 11:27:21 -0300

    test: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login/login.spec.ts
+++ src/presentation/controllers/login/login/login.spec.ts
@@ -1,14 +1,27 @@
-import { expect, test, describe } from '@jest/globals'
 import { LoginController } from './login'
 import { MissingParamError } from '../../../errors'
+import { AddAccountParams } from './../../../../domain/usecases/account/add-account'
+import { Authentication } from './../../../../domain/usecases/account/authentication'
+import { expect, test, describe, jest } from '@jest/globals'
+
+const mockAuthentication = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth (data: AddAccountParams): Promise<string> {
+      return await Promise.resolve('any_token')
+    }
+  }
+  return new AuthenticationStub()
+}
 
 type SutTypes = {
   sut: LoginController
+  authenticationStub: Authentication
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController()
-  return { sut }
+  const authenticationStub = mockAuthentication()
+  const sut = new LoginController(authenticationStub)
+  return { sut, authenticationStub }
 }
 
 describe('Login Controller', () => {
@@ -33,5 +46,18 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('should Login Controller calls Authentication with correct values', async () => {
+    const { sut, authenticationStub } = makeSut()
+    const addSpy = jest.spyOn(authenticationStub, 'auth')
+    const httpRequest = {
+      body: {
+        username: 'any_username',
+        password: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
