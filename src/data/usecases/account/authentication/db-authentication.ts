19d9aef69f9ff50216d825d5c67bdd39d49ffe8f
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-10 16:29:48 -0300

    feat: ensure DbAuthentication returns null if  returns null

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -5,7 +5,8 @@ import { AddAccountParams } from '../add-account/db-add-account-protocols'
 export class DbAuthentication implements Authentication {
   constructor (private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository) {}
   async auth (data: AddAccountParams): Promise<string | null> {
-    this.loadUserByUsernameRepository.loadByUsername(data.username)
+    const account = await this.loadUserByUsernameRepository.loadByUsername(data.username)
+    if (!account) return null
     return await Promise.resolve(null)
   }
 }
