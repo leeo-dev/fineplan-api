d1cc68e82f3367e2fe27abb113b32b57e8d0494c
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-10 16:59:24 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -1,12 +1,17 @@
+import { HashComparer } from './../../../protocols/hash-comparer'
 import { LoadUserByUsernameRepository } from './../../../protocols/load-user-by-username-repository'
 import { Authentication } from '@/domain/usecases/account/authentication'
 import { AddAccountParams } from '../add-account/db-add-account-protocols'
 
 export class DbAuthentication implements Authentication {
-  constructor (private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository) {}
+  constructor (
+    private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository,
+    private readonly hashComparer: HashComparer) {}
+
   async auth (data: AddAccountParams): Promise<string | null> {
     const account = await this.loadUserByUsernameRepository.loadByUsername(data.username)
     if (!account) return null
+    await this.hashComparer.compare(data.password, account.password)
     return await Promise.resolve(null)
   }
 }
