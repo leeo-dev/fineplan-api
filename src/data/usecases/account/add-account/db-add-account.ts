7d164ba2ad1dbcfd9084c8e6be5b45576a83fc2b
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-13 10:24:29 -0300

    feat: ensure DbAddAccount calls Encrypter with correct id

--- src/data/usecases/account/add-account/db-add-account.ts
+++ src/data/usecases/account/add-account/db-add-account.ts
@@ -1,10 +1,12 @@
+import { Encrypter } from './../../../protocols/encrypter'
 import { AddAccountRepository, Hasher, LoadUserByUsernameRepository, AddAccount, AddAccountParams } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly loadUserByUsername: LoadUserByUsernameRepository,
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly encrypter: Encrypter
   ) {}
 
   async add (accountParams: AddAccountParams): Promise<string | null> {
@@ -12,8 +14,8 @@ export class DbAddAccount implements AddAccount {
     const account = await this.loadUserByUsername.loadByUsername(username)
     if (account) return null
     const hashPassword = await this.hasher.hash(password)
-    const newAccount = Object.assign({}, accountParams, { password: hashPassword })
-    const accountId = await this.addAccountRepository.add(newAccount)
+    const accountId = await this.addAccountRepository.add(Object.assign({}, accountParams, { password: hashPassword }))
+    this.encrypter.encrypt(accountId)
     return accountId
   }
 }
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,10 +2,14 @@ import { AddAccountMongoRepository } from '../../infra/db/account-mongo-reposito
 import { DbAddAccount } from '../../data/usecases/account/add-account/db-add-account'
 import { SignUpController } from '../../presentation/controllers/login/signup/signup'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { JwtAdapter } from '../../infra/cryptography/jwt-adapter'
+import env from '../config/env'
 export const makeSignUpController = (): SignUpController => {
+  const jwtAdapter = new JwtAdapter(env.JWT_SECRET)
+
   const salt = 12
   const mongoAccount = new AddAccountMongoRepository()
   const hasher = new BcryptAdapter(salt)
-  const dbAddAccount = new DbAddAccount(mongoAccount, hasher, mongoAccount)
+  const dbAddAccount = new DbAddAccount(mongoAccount, hasher, mongoAccount, jwtAdapter)
   return new SignUpController(dbAddAccount)
 }
