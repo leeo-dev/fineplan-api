fdad10df8d391ee28644d94e0946316bad5a2d13
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-15 17:55:06 -0300

    feat: ensure Deposit controller returns 400 if length of title is not between 3 and 25

--- src/presentation/controllers/transaction/deposit/deposit-controller.ts
+++ src/presentation/controllers/transaction/deposit/deposit-controller.ts
@@ -2,13 +2,19 @@ import { MissingParamError } from './../../../errors/missing-param-error'
 import { badRequest } from './../../../helpers/http/http'
 import { HttpRequest, HttpResponse } from '../../login/signup/signup-protocols'
 import { Controller } from './../../../protocols/controller'
+import { LengthParamError } from '@/presentation/errors'
 export class DepositController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['title', 'amount', 'date']
     for (const field of requiredFields) {
+      const lengthField = httpRequest.body[field]?.length
       if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      if (lengthField < 3 || lengthField > 25) return badRequest(new LengthParamError(field, 3, 25))
     }
 
-    return badRequest(new MissingParamError('title'))
+    return {
+      statusCode: 200,
+      body: {}
+    }
   }
 }
