db60196e4a9b7e9504499753057a362e7b3a28d4
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-15 17:21:16 -0300

    feat: ensure Deposit controller returns 400 if no amount is provided

--- src/presentation/controllers/transaction/deposit/deposit-controller.ts
+++ src/presentation/controllers/transaction/deposit/deposit-controller.ts
@@ -4,6 +4,11 @@ import { HttpRequest, HttpResponse } from '../../login/signup/signup-protocols'
 import { Controller } from './../../../protocols/controller'
 export class DepositController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['title', 'amount']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+    }
+
     return badRequest(new MissingParamError('title'))
   }
 }
