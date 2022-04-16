8f8328a09078c12fd7f13116de6dccbc8a4c6950
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-16 16:13:42 -0300

    feat: ensure Deposit controller returns 400 if amount is less or equal zero

--- src/presentation/controllers/transaction/deposit/deposit-controller.ts
+++ src/presentation/controllers/transaction/deposit/deposit-controller.ts
@@ -4,17 +4,18 @@ import { Controller } from './../../../protocols/controller'
 import { InvalidParamError, LengthParamError, MissingParamError } from '@/presentation/errors'
 export class DepositController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['title', 'amount', 'date']
+    const requiredFields = ['title', 'date', 'amount']
     for (const field of requiredFields) {
       const lengthField = httpRequest.body[field]?.length
       if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      if (field === 'amount') break
       if (lengthField < 3 || lengthField > 25) return badRequest(new LengthParamError(field, 3, 25))
     }
 
     const { amount } = httpRequest.body
 
     const isInvalidAmount = isNaN(amount)
-    if (isInvalidAmount) return badRequest(new InvalidParamError('amount'))
+    if (isInvalidAmount || amount <= 0) return badRequest(new InvalidParamError('amount'))
 
     return {
       statusCode: 200,
