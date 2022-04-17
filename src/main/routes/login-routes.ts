02524f24a1001bcfde25e1ada4bd9278e5ca538f
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-17 10:21:22 -0300

    refactor: orgnize folders factory

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeLoginController } from '../factories/login'
+import { makeLoginController } from '../factories/login/login'
 
 export default (router: Router): void => {
   router.post('/login', adaptRoute(makeLoginController()))
