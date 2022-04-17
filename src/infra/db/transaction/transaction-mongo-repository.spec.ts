878d8c4d96d121464c4b681d7e17254d15062053
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-17 10:18:41 -0300

    test: ensure TransactionMongoRepository insert a transaction successfully

---
+++ src/infra/db/transaction/transaction-mongo-repository.spec.ts
@@ -0,0 +1,40 @@
+import { TransactionMongoRepository } from './transaction-mongo-repository'
+import { MongoHelper } from './../../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import MockDate from 'mockdate'
+
+import { expect, test, describe, beforeAll, afterAll, beforeEach } from '@jest/globals'
+
+const makeSut = (): TransactionMongoRepository => {
+  return new TransactionMongoRepository()
+}
+
+describe('Name of the group', () => {
+  let transactionCollection: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+    MockDate.set(new Date())
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    MockDate.reset()
+  })
+
+  beforeEach(async () => {
+    transactionCollection = await MongoHelper.getCollection('transactions')
+    await transactionCollection.deleteMany({})
+  })
+  test('Should TransactionMongoRepository insert a transaction successfully', async () => {
+    const sut = makeSut()
+    await sut.add({ title: 'any_title', amount: 250, date: new Date('2020-05-05'), created_at: new Date() })
+    const transactionMongo = await transactionCollection.findOne({ title: 'any_title' })
+    const transaction = MongoHelper.map(transactionMongo)
+    expect(transaction).toBeTruthy()
+    expect(transaction?.id).toBeTruthy()
+    expect(transaction?.title).toBeTruthy()
+    expect(transaction?.amount).toBeTruthy()
+    expect(transaction?.date).toBeTruthy()
+    expect(transaction?.created_at).toBeTruthy()
+  })
+})
