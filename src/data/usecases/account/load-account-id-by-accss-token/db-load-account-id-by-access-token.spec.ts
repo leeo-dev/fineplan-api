d64da4bd23c2c5f00836bf705d383f59ac53ba71
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-18 10:03:37 -0300

    test: ensure DbLoadAccountIdByAccessToken call Decrypter with correct values

---
+++ src/data/usecases/account/load-account-id-by-accss-token/db-load-account-id-by-access-token.spec.ts
@@ -0,0 +1,31 @@
+import { DbLoadAccountIdByAccessToken } from './db-load-account-id-by-access-token'
+import { Decrypter } from './../../../protocols/decrypter'
+import { expect, test, describe, jest } from '@jest/globals'
+
+const mockDecrypter = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    decrypt (accessToken: string): string {
+      return 'any_id'
+    }
+  }
+  return new DecrypterStub()
+}
+
+type SutTypes = {
+  sut: DbLoadAccountIdByAccessToken
+  decrypterStub: Decrypter
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = mockDecrypter()
+  const sut = new DbLoadAccountIdByAccessToken(decrypterStub)
+  return { sut, decrypterStub }
+}
+describe('DbLoadAccountIdByAccessToken', () => {
+  test('Should call decrypter with correct token', async () => {
+    const { sut, decrypterStub } = makeSut()
+    const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
+    await sut.loadIdByAccessToken('any_accessToken')
+    expect(decryptSpy).toHaveBeenCalledWith('any_accessToken')
+  })
+})
