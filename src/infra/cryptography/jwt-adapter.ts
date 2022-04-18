95ad9f83aa8755856b3e4d5d655b3bd8c4612d59
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-18 10:59:08 -0300

    feat: ensure JWT Adapter calls jwt veridy with correct token

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -1,9 +1,15 @@
+import { Decrypter } from './../../data/protocols/decrypter'
 import { Encrypter } from './../../data/protocols/encrypter'
 import jwt from 'jsonwebtoken'
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
   async encrypt (value: string): Promise<string> {
     const token = await jwt.sign({ id: value }, this.secret)
     return token
+  }
+
+  decrypt (accessToken: string): string | null {
+    jwt.verify(accessToken, this.secret)
+    return null
   }
 }
