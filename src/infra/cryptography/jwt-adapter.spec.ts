6aa7bb72f07df2d2cd2ed1b00e6bda69b260a54b
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-18 10:59:55 -0300

    test: ensure JWT Adapter calls jwt verify with correct token

--- src/infra/cryptography/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter.spec.ts
@@ -6,6 +6,9 @@ const secret = 'any_secret'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return await Promise.resolve('any_token')
+  },
+  async verify (): Promise<string> {
+    return await Promise.resolve('any_id')
   }
 }))
 
@@ -14,15 +17,30 @@ const makeSut = (): JwtAdapter => {
 }
 
 describe('JWT Adapter', () => {
-  test('Should call JWT with correct values', async () => {
-    const sut = makeSut()
-    const signSpy = jest.spyOn(jwt, 'sign')
-    sut.encrypt('any_id')
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, secret)
+  describe('Encrypt', () => {
+    test('Should call JWT with correct values', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      sut.encrypt('any_id')
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, secret)
+    })
+    test('Should return a token on success', async () => {
+      const sut = makeSut()
+      const accessToken = await sut.encrypt('any_value')
+      expect(accessToken).toBe('any_token')
+    })
   })
-  test('Should return a token on success', async () => {
-    const sut = makeSut()
-    const accessToken = await sut.encrypt('any_value')
-    expect(accessToken).toBe('any_token')
+  describe('decrypt()', () => {
+    test('Should call JWT with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', secret)
+    })
   })
+  // test('Should return a token on success', async () => {
+  //   const sut = makeSut()
+  //   const accessToken = await sut.encrypt('any_value')
+  //   expect(accessToken).toBe('any_token')
+  // })
 })
