a1bf7d48b917c26d9f0107c20126d97737c0a51e
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-19 22:54:05 -0300

    test: ensure LengthFieldValidator returns LengthParamError if validator fails

---
+++ src/validation/validators/length-field-validator.spec.ts
@@ -0,0 +1,11 @@
+import { LengthFieldValidator } from './length-field-validator'
+import { LengthParamError } from './../../presentation/errors'
+import { expect, test, describe } from '@jest/globals'
+
+describe('Length Field Validator', () => {
+  test('should returns LengthParamError if validation fails', () => {
+    const sut = new LengthFieldValidator('name')
+    const error = sut.validate({ name: 'na' })
+    expect(error).toEqual(new LengthParamError('name'))
+  })
+})
