8a2bde820ecdd4351bdbf04e166fbafa0390ee9e
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-19 22:29:32 -0300

    feat: requiredFieldValidator returns MissingParamError if validation fails

---
+++ src/validation/validators/required-fields-validator.ts
@@ -0,0 +1,11 @@
+import { MissingParamError } from './../../presentation/errors/missing-param-error'
+import { Validation } from './../../presentation/protocols/validation'
+export class RequiredFieldsValidator implements Validation {
+  constructor (private readonly fieldName: string) {}
+  validate (input: any): Error | null {
+    if (!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName)
+    }
+    return null
+  }
+}
