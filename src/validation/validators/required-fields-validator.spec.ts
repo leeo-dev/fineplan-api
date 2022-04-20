05438cb8a617869c02c452b6821a334ec755bc0f
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-19 22:29:50 -0300

    test: requiredFieldValidator returns MissingParamError if validation fails

---
+++ src/validation/validators/required-fields-validator.spec.ts
@@ -0,0 +1,11 @@
+import { MissingParamError } from './../../presentation/errors/missing-param-error'
+import { expect, test, describe } from '@jest/globals'
+import { RequiredFieldsValidator } from './required-fields-validator'
+
+describe('Required Fields Validator', () => {
+  test('should returns MissingParamError if validation fails', () => {
+    const sut = new RequiredFieldsValidator('name')
+    const error = sut.validate({})
+    expect(error).toEqual(new MissingParamError('name'))
+  })
+})
