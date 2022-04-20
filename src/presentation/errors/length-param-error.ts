1ba00cc093bdb7a8fa61dc8d3cdd4de66e501df9
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-19 22:53:42 -0300

    feat: ensure LengthFieldValidator returns LengthParamError if validator fails

--- src/presentation/errors/length-param-error.ts
+++ src/presentation/errors/length-param-error.ts
@@ -1,5 +1,5 @@
 export class LengthParamError extends Error {
-  constructor (paramName: string, min: number, max: number) {
+  constructor (paramName: string, min: number = 3, max: number = 25) {
     super(`${paramName} needs to be between ${min} to ${max} characters`)
     this.name = 'LengthParamError'
   }
---
+++ src/validation/validators/length-field-validator.ts
@@ -0,0 +1,12 @@
+import { LengthParamError } from './../../presentation/errors'
+import { Validation } from './../../presentation/protocols/validation'
+export class LengthFieldValidator implements Validation {
+  constructor (private readonly fieldName: string,
+    private readonly minLength: number = 3,
+    private readonly maxLength: number = 25) {}
+
+  validate (input: any): Error | null {
+    if (input[this.fieldName]?.length < 3 || input[this.fieldName]?.length > 25) return new LengthParamError(this.fieldName, this.minLength, this.maxLength)
+    return null
+  }
+}
