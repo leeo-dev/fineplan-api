c3ac0cd5be72b8d64375a32961458f43280e696b
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-21 11:18:49 -0300

    refactor: change paths name

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeSignUpController } from '../factories/login/signup-controller-factory'
+import { makeSignUpController } from '../factories/login/signup/signup-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
