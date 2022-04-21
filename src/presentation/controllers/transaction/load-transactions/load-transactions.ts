8c6f4ac8d8708a005d9f413f18fabda6e6b0db21
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-21 20:51:03 -0300

    feat: ensure LoadTransactionsController return 500 if LoadTransaction throws

--- src/presentation/controllers/transaction/load-transactions/load-transactions.ts
+++ src/presentation/controllers/transaction/load-transactions/load-transactions.ts
@@ -1,13 +1,17 @@
-import { ok, noContent } from './../../../helpers/http/http'
+import { ok, noContent, serverError } from './../../../helpers/http/http'
 import { LoadTransactions } from './../../../../domain/usecases/transaction/load-transactions'
 import { Controller, HttpRequest, HttpResponse } from '../deposit/deposit-controller-protocols'
 
 export class LoadTransactionsController implements Controller {
   constructor (private readonly loadTransactions: LoadTransactions) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const user = httpRequest.user
+    try {
+      const user = httpRequest.user
 
-    const transactions = await this.loadTransactions.loadAll(String(user?.id))
-    return transactions.length ? ok(transactions) : noContent()
+      const transactions = await this.loadTransactions.loadAll(String(user?.id))
+      return transactions.length ? ok(transactions) : noContent()
+    } catch (error: any) {
+      return serverError(error)
+    }
   }
 }
