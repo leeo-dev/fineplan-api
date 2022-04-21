3f150c8ceed31a38516d90516d7e7f76f593a5a3
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-21 11:03:19 -0300

    feat: ensure LoginValidationFactory calls ValidationComposit with correct validations

---
+++ src/main/factories/login/login/login-controller-factory.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite } from './../../../../validation/validation-composite'
+import { JwtAdapter } from '../../../../infra/cryptography/jwt-adapter'
+import { BcryptAdapter } from '../../../../infra/cryptography/bcrypt-adapter'
+import { AddAccountMongoRepository } from '../../../../infra/db/account/account-mongo-repository'
+import { DbAuthentication } from '../../../../data/usecases/account/authentication/db-authentication'
+import { LoginController } from '../../../../presentation/controllers/login/login/login'
+import env from '../../../config/env'
+export const makeLoginController = (): LoginController => {
+  const salt = 12
+  const jwtAdapter = new JwtAdapter(env.JWT_SECRET)
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const LoadUserByUsernameRepository = new AddAccountMongoRepository()
+  const authentication = new DbAuthentication(LoadUserByUsernameRepository, bcryptAdapter, jwtAdapter)
+  const validation = new ValidationComposite([])
+  return new LoginController(authentication, validation)
+}
