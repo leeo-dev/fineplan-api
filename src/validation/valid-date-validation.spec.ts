12921176b53f7129f58a512f0e42a9c9ade38ce4
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-21 11:47:56 -0300

    test: ensure ValidDateValidation returns null if validation succeeds

--- src/validation/valid-date-validation.spec.ts
+++ src/validation/valid-date-validation.spec.ts
@@ -5,7 +5,12 @@ import { expect, test, describe } from '@jest/globals'
 describe('Valid Amount Validator', () => {
   test('should returns LengthParamError if validation fails', () => {
     const sut = new ValidDateValidation('date')
-    const error = sut.validate({ amount: 'invalid_date' })
+    const error = sut.validate({ date: 'invalid_date' })
     expect(error).toEqual(new InvalidParamError('date'))
+  })
+  test('should returns null if validation succeeds', () => {
+    const sut = new ValidDateValidation('date')
+    const error = sut.validate({ date: '2020-05-05' })
+    expect(error).toBeNull()
   })
 })
