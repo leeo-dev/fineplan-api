72171c49fc9890362c49a3ea117293f0a753e49f
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-21 11:04:04 -0300

    test: ensure LoginValidationFactory calls ValidationComposit with correct validations

---
+++ src/main/factories/login/login/login-validation-factory.spec.ts
@@ -0,0 +1,21 @@
+import { makeLoginValidation } from './login-validation-factory'
+import { LengthFieldValidator } from './../../../../validation/length-field-validator'
+import { RequiredFieldsValidator } from './../../../../validation/required-fields-validator'
+import { Validation } from './../../../../presentation/protocols/validation'
+import { ValidationComposite } from './../../../../validation/validation-composite'
+import { expect, test, describe, jest } from '@jest/globals'
+
+jest.mock('../../../../validation/validation-composite')
+
+describe('LoginValidation Factory', () => {
+  test('should call Validation Composite with all validations', () => {
+    const validations: Validation[] = []
+    const requiredFields = ['username', 'password']
+    for (const field of requiredFields) {
+      validations.push(new RequiredFieldsValidator(field))
+      validations.push(new LengthFieldValidator(field, 3, 25))
+    }
+    makeLoginValidation()
+    expect(ValidationComposite).toHaveBeenLastCalledWith(validations)
+  })
+})
