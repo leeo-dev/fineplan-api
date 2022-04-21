d484f268fa1cc71ddcf13918ee484a6950f1ad80
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-21 12:06:14 -0300

    test: ensure DepositController returns 400 if validation fails

--- src/presentation/controllers/transaction/deposit/deposit-controller.spec.ts
+++ src/presentation/controllers/transaction/deposit/deposit-controller.spec.ts
@@ -1,7 +1,7 @@
 import { Validation } from './../../../protocols/validation'
 import { DepositController } from './deposit-controller'
 import { AddTransaction, TransactionParam } from './deposit-controller-protocols'
-import { LengthParamError, MissingParamError, InvalidParamError } from '../../../errors'
+import { MissingParamError } from '../../../errors'
 import { badRequest, noContent } from './../../../helpers/http/http'
 import { expect, test, describe, jest, beforeAll, afterAll } from '@jest/globals'
 import MockDate from 'mockdate'
@@ -64,86 +64,20 @@ describe('Deposit Controller', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
-  test('Should return 400 if no title is provided', async () => {
-    const { sut } = makeSut()
+  test('Should return 400 if  ValidationComposite fails', async () => {
+    const { sut, validationCompositeStub } = makeSut()
+    jest.spyOn(validationCompositeStub, 'validate').mockReturnValueOnce(new MissingParamError('title'))
     const httpRequest = {
       body: {
-        amount: 'any_amount',
-        date: 'any_date'
+        amount: 250,
+        date: '2020-05-05'
+      },
+      user: {
+        id: 'any_id'
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('title')))
-  })
-  test('Should return 400 if no amount is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        title: 'any_title',
-        date: 'any_date'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('amount')))
-  })
-  test('Should return 400 if no date is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        title: 'any_title',
-        amount: 'any_amount'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('date')))
-  })
-  test('Should return 400 if length of title is not between 3 and 25 characters', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        title: 'an',
-        amount: 'any_amount',
-        date: 'any_date'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new LengthParamError('title', 3, 25)))
-  })
-  test('Should return 400 if amount is not a number', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        title: 'any_title',
-        amount: 'invalid_type',
-        date: 'any_date'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('amount')))
-  })
-  test('Should return 400 if amount less or equal than zero', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        title: 'any_title',
-        amount: '0',
-        date: 'any_date'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('amount')))
-  })
-  test('Should return 400 if amount less or equal than zero', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        title: 'any_title',
-        amount: '250',
-        date: 'any_date'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('date')))
   })
   test('Should call AddTransaction with correct values', async () => {
     const { sut, addTransactionStub } = makeSut()
