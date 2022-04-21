0c7dfbb0dacf0a3c10c9cd4ac847038b3f1c25aa
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-21 10:36:28 -0300

    feat: ensure SignUpController returns 400 if Validation Compsite fails

--- src/presentation/controllers/login/signup/signup.ts
+++ src/presentation/controllers/login/signup/signup.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, HttpResponse, Controller, Validation } from './signup-protocols'
 import { badRequest, forbidden, ok, serverError } from '../../../helpers/http/http'
-import { UsernameInUseError, MissingParamError, LengthParamError } from '../../../errors'
+import { UsernameInUseError } from '../../../errors'
 import { AddAccount } from '../../../../domain/usecases/account/add-account'
 export class SignUpController implements Controller {
   constructor (private readonly addAccount: AddAccount,
@@ -9,17 +9,8 @@ export class SignUpController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise <HttpResponse> {
     try {
-      this.validationComposite.validate(httpRequest.body)
-      const requiredFields = ['username', 'password']
-      for (const field of requiredFields) {
-        const lengthField = httpRequest.body[field]?.length
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
-        if (lengthField < 3 || lengthField > 25) {
-          return badRequest(new LengthParamError(field, 3, 25))
-        }
-      }
+      const error = this.validationComposite.validate(httpRequest.body)
+      if (error) return badRequest(error)
       const { username, password } = httpRequest.body
       const accessToken = await this.addAccount.add({ username, password })
       if (!accessToken) return forbidden(new UsernameInUseError())
