698e39e081c533dba7f09e101c89c812c6c92912
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-21 21:30:26 -0300

    test: ensure LoadTransactionsRepository returns list of transactions on success

--- src/infra/db/transaction/transaction-mongo-repository.spec.ts
+++ src/infra/db/transaction/transaction-mongo-repository.spec.ts
@@ -1,5 +1,5 @@
 import { TransactionMongoRepository } from './transaction-mongo-repository'
-import { TransactionParam } from './transaction-mongo-repository-protocols'
+import { TransactionModel, TransactionParam } from './transaction-mongo-repository-protocols'
 import { MongoHelper } from './../../helpers/mongo-helper'
 
 import { Collection } from 'mongodb'
@@ -20,7 +20,12 @@ const mockTransaction = (type: string): TransactionParam => ({
   user_id: 'any_id'
 })
 
-describe('Name of the group', () => {
+const mockTransactions = (): TransactionModel[] => ([
+  { id: 'any_id', title: 'any_title', amount: -250, date: new Date(), type: 'withdraw', created_at: new Date(), user_id: 'any_user_id' },
+  { id: 'other_id', title: 'other_title', amount: 1200, date: new Date(), type: 'deposit', created_at: new Date(), user_id: 'other_user_id' }
+])
+
+describe('Transaction Mongo Repository', () => {
   let transactionCollection: Collection
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL ?? '')
@@ -36,16 +41,26 @@ describe('Name of the group', () => {
     transactionCollection = await MongoHelper.getCollection('transactions')
     await transactionCollection.deleteMany({})
   })
-  test('Should TransactionMongoRepository insert a transaction successfully', async () => {
-    const sut = makeSut()
-    await sut.add(mockTransaction('deposit'))
-    const transactionMongo = await transactionCollection.findOne({ title: 'any_title' })
-    const transaction = MongoHelper.map(transactionMongo)
-    expect(transaction).toBeTruthy()
-    expect(transaction?.id).toBeTruthy()
-    expect(transaction?.title).toBeTruthy()
-    expect(transaction?.amount).toBeTruthy()
-    expect(transaction?.date).toBeTruthy()
-    expect(transaction?.created_at).toBeTruthy()
+  describe('add()', () => {
+    test('Should TransactionMongoRepository insert a transaction successfully', async () => {
+      const sut = makeSut()
+      await sut.add(mockTransaction('deposit'))
+      const transactionMongo = await transactionCollection.findOne({ title: 'any_title' })
+      const transaction = MongoHelper.map(transactionMongo)
+      expect(transaction).toBeTruthy()
+      expect(transaction?.id).toBeTruthy()
+      expect(transaction?.title).toBeTruthy()
+      expect(transaction?.amount).toBeTruthy()
+      expect(transaction?.date).toBeTruthy()
+      expect(transaction?.created_at).toBeTruthy()
+    })
+  })
+  describe('loadAll()', () => {
+    test('Should TransactionMongoRepository insert a transaction successfully', async () => {
+      await transactionCollection.insertMany(mockTransactions())
+      const sut = makeSut()
+      const transactions = await sut.loadAll('any_user_id')
+      expect(transactions).toBeTruthy()
+    })
   })
 })
