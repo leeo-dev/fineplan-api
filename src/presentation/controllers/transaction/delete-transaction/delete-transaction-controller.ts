ba6d354127408f65bf19a3047520d17064c00f81
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-22 10:33:25 -0300

    feat: ensure DeleteTransactionController returns 403 if no user is founded

--- src/presentation/controllers/transaction/delete-transaction/delete-transaction-controller.ts
+++ src/presentation/controllers/transaction/delete-transaction/delete-transaction-controller.ts
@@ -1,11 +1,11 @@
 import { Controller, HttpRequest, HttpResponse, DeleteTransaction } from './delete-transaction-controller-protocols'
 import { AccessDeniedError } from './../../../errors/access-denied-error'
-import { badRequest } from './../../../helpers/http/http'
+import { forbidden } from './../../../helpers/http/http'
 export class DeleteTransactionController implements Controller {
   constructor (private readonly deleteTransaction: DeleteTransaction) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { user } = httpRequest
-    if (!user) badRequest(new AccessDeniedError())
+    if (!user) return forbidden(new AccessDeniedError())
     const userId = String(user?.id)
     await this.deleteTransaction.delete(userId)
     return { statusCode: 200, body: null }
