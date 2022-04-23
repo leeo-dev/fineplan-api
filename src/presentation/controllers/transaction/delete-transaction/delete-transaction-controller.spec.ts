ad441f080cfda174b28cbeb7a2252a2a6db4a3e6
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-22 22:42:15 -0300

    test: ensure DeleteTransactionControllers returns 400 if no id is provided

--- src/presentation/controllers/transaction/delete-transaction/delete-transaction-controller.spec.ts
+++ src/presentation/controllers/transaction/delete-transaction/delete-transaction-controller.spec.ts
@@ -1,6 +1,6 @@
 import { DeleteTransactionController } from './delete-transaction-controller'
 import { DeleteTransaction } from './delete-transaction-controller-protocols'
-import { noContent, serverError, notFound } from './../../../helpers/http/http'
+import { noContent, serverError, notFound, badRequest } from './../../../helpers/http/http'
 import { MissingParamError } from './../../../errors/missing-param-error'
 import { expect, test, describe, jest } from '@jest/globals'
 
@@ -42,6 +42,11 @@ describe('DeleteTransaction Controller', () => {
 
     const httpResponse = await sut.handle({ params: { id: 'any_id' } })
     expect(httpResponse).toEqual(notFound(new MissingParamError('id')))
+  })
+  test('Should return 400 if no params is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({ params: {} })
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('id')))
   })
   test('Should throw if DeleteTransaction throws', async () => {
     const { sut, deleteTransactionStub } = makeSut()
