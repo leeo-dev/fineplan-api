3d0a73afece0e19a9c03ca43d81477e84d82ce08
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-22 22:01:38 -0300

    test: ensure DeleteTransactionControllers reutrn 404 if DeleteTransaction returns false

--- src/presentation/controllers/transaction/delete-transaction/delete-transaction-controller.spec.ts
+++ src/presentation/controllers/transaction/delete-transaction/delete-transaction-controller.spec.ts
@@ -1,11 +1,13 @@
 import { DeleteTransactionController } from './delete-transaction-controller'
 import { DeleteTransaction } from './delete-transaction-controller-protocols'
-import { noContent, serverError } from './../../../helpers/http/http'
+import { noContent, serverError, notFound } from './../../../helpers/http/http'
+import { MissingParamError } from './../../../errors/missing-param-error'
 import { expect, test, describe, jest } from '@jest/globals'
 
 const mockDeleteTransaction = (): DeleteTransaction => {
   class DeleteTransactionStub implements DeleteTransaction {
-    async delete (id: string): Promise<void> {
+    async delete (id: string): Promise<boolean> {
+      return true
     }
   }
   return new DeleteTransactionStub()
@@ -33,6 +35,13 @@ describe('DeleteTransaction Controller', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({ params: { id: 'any_id' } })
     expect(httpResponse).toEqual(noContent())
+  })
+  test('Should return 404 if DeleteTransaction return false', async () => {
+    const { sut, deleteTransactionStub } = makeSut()
+    jest.spyOn(deleteTransactionStub, 'delete').mockReturnValueOnce(Promise.resolve(false))
+
+    const httpResponse = await sut.handle({ params: { id: 'any_id' } })
+    expect(httpResponse).toEqual(notFound(new MissingParamError('id')))
   })
   test('Should throw if DeleteTransaction throws', async () => {
     const { sut, deleteTransactionStub } = makeSut()
