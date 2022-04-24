290c0b93c537a3dfe4ec27eeee3f4e7ee28fe682
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-04-23 21:46:45 -0300

    feat: ensure UpdateTransactionController returns 500 if UpdateTransaction throws

--- src/presentation/controllers/transaction/update-transaction/update-transaction-controller.ts
+++ src/presentation/controllers/transaction/update-transaction/update-transaction-controller.ts
@@ -1,5 +1,5 @@
 import { MissingParamError } from './../../../errors/missing-param-error'
-import { badRequest, notFound } from './../../../helpers/http/http'
+import { badRequest, notFound, serverError, ok } from './../../../helpers/http/http'
 import { Validation } from './../../../protocols/validation'
 import { UpdateTransaction } from './../../../../domain/usecases/transaction/update-transaction'
 import { HttpRequest, HttpResponse } from '../deposit/deposit-controller-protocols'
@@ -11,18 +11,18 @@ export class UpdateTransactionController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validationComposite.validate(httpRequest.body)
-    if (error) return badRequest(error)
-    const { title, type, amount, date } = httpRequest.body
-    const { id } = httpRequest.params
-    const user = httpRequest.user
-    if (!user) return badRequest(new MissingParamError('user_id'))
-    const userId = String(user.id)
-    const transaction = await this.updateTransaction.update({ id, title, type, amount, date, user_id: userId })
-    if (!transaction) return notFound(new MissingParamError('id'))
-    return {
-      statusCode: 200,
-      body: null
+    try {
+      const error = this.validationComposite.validate(httpRequest.body)
+      if (error) return badRequest(error)
+      const { title, type, amount, date } = httpRequest.body
+      const { id } = httpRequest.params
+      const user = httpRequest.user
+      if (!user) return badRequest(new MissingParamError('user_id'))
+      const transaction = await this.updateTransaction.update({ id, title, type, amount, date, user_id: String(user.id) })
+      if (!transaction) return notFound(new MissingParamError('id'))
+      return ok(null)
+    } catch (error: any) {
+      return serverError(error)
     }
   }
 }
